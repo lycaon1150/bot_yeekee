33e07d0ddb7c2cc8ad591ab81c0f098666ff5399
Debian <bitnami@ip-172-26-1-25.ap-southeast-1.compute.internal>
2025-04-08 07:04:53 +0700

    v2.08 fix delay

--- connection.py
+++ connection.py
@@ -828,7 +828,7 @@ class yeekee_bot(object):
                             # self.driver.refresh()
 
                                 
-                            sleep(1.5)
+                            sleep(1.8)
                             
                             ######### ยิงเลขครั้งแรก ##############
                             if money == 'yes':
