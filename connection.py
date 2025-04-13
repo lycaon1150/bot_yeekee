0acaa930935d039edf7c5052a323cce19a7a68e3
Debian <bitnami@ip-172-26-1-25.ap-southeast-1.compute.internal>
2025-04-14 06:05:49 +0700

    v2.08 fix delay

--- connection.py
+++ connection.py
@@ -841,7 +841,7 @@ class yeekee_bot(object):
                             elif this_host == 'thailotto' and bet_type == 'normal' and money == 'no':
                                 sleep(7)
                                 now = datetime.datetime.now() 
-                                self.driver.execute_script(js_send_number) 
+                                # self.driver.execute_script(js_send_number) 
                                 print('done ckick 1st')
                                 print(now)
                                 sleep(2)
